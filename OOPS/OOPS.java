25ced36a28e3ae7aeee359b38bb4ecc8f88b45f4
Akshay Patil <shivanagouda941@gmail.com>
2026-04-23 22:43:18 +0530

    Abstraction

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -38,6 +38,34 @@ public class OOPS{
     //A constructor is a special method used to initialize objects.(also allocate memory for object)
     // we use getter and setter to use attribute outside class if attribut is NOT public
     // destructor- Garbage Collector
+
+
+
+    abstract class Animal {
+    // Abstract method (no body)
+    abstract void sound(); //iead
+
+    // Normal method
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
+
+class Dog extends Animal {
+    // Providing implementation
+    void sound() {   //implementation
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Dog();  // reference of parent, object of child
+
+        a.sound();  // calls Dog's method
+        a.sleep();  // calls parent method
+    }
+}
     public static void main(String[] args) {
     
     }
